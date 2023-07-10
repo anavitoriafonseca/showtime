e94a924e66e6bca680a77337d58d0ba02a638ea6
anavitoriafonseca <anavitoria.aninha@gmail.com>
2023-07-10 20:04:13 -0300

    day one

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,25 +1,35 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+//    var text by remember { mutableStateOf("Hello, World!") }
 
     MaterialTheme {
-        Button(onClick = {
+        /*Button(onClick = {
             text = "Hello, Desktop!"
         }) {
             Text(text)
+        }*/
+        Column {
+            Text("Me Before you")
+            Image(
+                painter = painterResource("meBeforeYou.webp"),
+                contentDescription = "Movie",
+                modifier = Modifier.fillMaxSize(0.5F)
+            )
+            Text("Ano: 2016 -  Nota: 7.4")
         }
     }
 }
