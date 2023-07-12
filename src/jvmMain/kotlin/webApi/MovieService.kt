aa0b7e43e0b9f44c47eab4d5ebc749874b1f4d2b
anavitoriafonseca <anavitoria.aninha@gmail.com>
2023-07-12 11:44:53 -0300

    add retrofit

---
+++ src/jvmMain/kotlin/webApi/MovieService.kt
@@ -0,0 +1,8 @@
+package webApi
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+interface MovieService {
+    @GET("top250.json")
+    fun findTop250Movies(): Call<ResponseBody>
+}
